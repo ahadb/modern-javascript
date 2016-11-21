627a927b2089deb2e33499ad2946702b247f6702
Ahad Bokhari <ahadbokhari@gmail.com>
2016-11-21 12:03:18 -0600

    coding and naming conventions

---
+++ general-patterns/coding-and-naming-conventions.js
@@ -0,0 +1,38 @@
+/**
+ * Coding & Naming Conventions
+ * JavaScript Patterns - modern JS patterns with ES5 & ES6 examples
+ *
+ * @copyright 2016 Ahad Bokhari
+ * license: MIT
+ */
+
+/* (i.) Be courteous to those who read your code and allow for context in your
+ * naming - be descriptive and try to avoid one letter names
+ */
+function initialize() {
+  // ...do stuff...
+}
+
+// a. anti-pattern
+function i() {
+  // ...do stuff...
+}
+
+/* (ii.) Variables should be declared before they are used and should be descriptive.
+ * preferably variables should be alphabetical order
+ */
+const age = 0;
+const adult = true;
+const gender = '';
+const isTeenager = !!adult;
+const location = {};
+
+
+/* (iii.) When to use camel vs pascal case
+
+
+/* (iv.) Function declarations */
+// classes
+
+
+/* (v.) Statements
