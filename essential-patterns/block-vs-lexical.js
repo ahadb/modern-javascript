2fbb1e8474ab25008c8a19a8fafd2c3bdb089ea9
Ahad Bokhari <ahadbokhari@gmail.com>
2017-02-13 19:18:53 -0500

    more scope stuff

--- essential-patterns/block-vs-lexical.js
+++ essential-patterns/block-vs-lexical.js
@@ -10,10 +10,12 @@
  * defined at author time by the programmer. Lexical scoping defines how variable     * names are resolved in nested functions: inner functions contain the scope of parent * functions even if the parent function has returned.
  */
 
+// a. Take a look at this block of code
+
 // function foo is the global scope
 function foo(x) {
   // this encompasses the scope of foo
-  const y = x * 5;
+  var y = x * 5;
 
   function baz(z) {
     // encompasses the scope of bar
@@ -22,4 +24,44 @@ function foo(x) {
   baz(y * 5);
 }
 
-foo(2); // => 2 10 50
+foo(2);
+// => 2 10 50
+
+// b. Scope based hiding techniques are powerful modules, do the scope of foo is nested,
+// ultimately hiding what gets defined inside of it from the outside world
+
+var a = 1;
+function foo() {
+  var a = 10;
+  console.log(a);
+}
+console.log(a);
+// => 1
+
+foo();
+// => 10
+
+// c. it's important to note that we can artificially introduce scope by creating new
+// functions and immediately invoking them.
+
+/* (ii). ES6 introduces block scoping and antything between { } introduces it's own
+ * scope. `let` statement declares a block scope local variable.
+ */
+
+// a. Consider the following code. Note that the { } not only creates new scope, it also
+// invokes the console.log statement
+
+{
+  let quux = "Hello World from ES6";
+  console.log(quux);
+  // => Hello World from ES6
+}
+
+{
+  let foo = "This has a different scope";
+  console.log(foo);
+  // => This has a different scope
+}
+
+console.log(quux);
+// => Reference Error: quux is not defined
