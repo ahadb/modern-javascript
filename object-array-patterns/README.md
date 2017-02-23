fec7482e6f1dfb49e0ff38530efb0dd1289edadf
Ahad Bokhari <ahadbokhari@gmail.com>
2017-02-22 21:52:16 -0500

    added obj creation pattern

---
+++ object-array-patterns/README.md
@@ -0,0 +1,94 @@
+# Table of Contents
+
+1. [Object Creation](#object-creation)
+
+## Object Creation 
+
+
+> (i.) JavaScript is designed around objects, a simple design pattern you should know well. An object is a collection of properties, and a property is an association
+  between a name (or key) and a value. A property's value can be a function, in which case the property is known as a method. Objects are crucial, if you understand
+  them, then you understand JavaScript
+
+2.1 An object is similar to variables, except for the properties associated with and attached to the object
+```javascript
+const myObj = new Object();
+
+// b. You may assign properties, out of which each will hold a value using dot notation
+
+myObj.age = 12;
+myObj.height = '173cm';
+myObj.color = 'Fair';
+myObj.ethnicity = 'Moroccan';
+
+console.log(myObj);
+
+/* ==>
+ age = 12;
+ height = '173cm';
+ color = 'Fair';
+ ethnicity = 'Moroccan';
+ */
+```
+
+2.2 You can also assign property and values, also known as key/value pairs via bracket notation
+```javascript
+myObj['key'] = 'Alicia';
+myObj['Always a string'] = 'String with spaces';
+myObj[''] = 'Empty string';
+
+console.log(myObj);
+
+/* ==>
+ age = 12;
+ height = '173cm';
+ color = 'Fair';
+ ethnicity = 'Moroccan';
+ key = 'Alicia';
+ Always a string = 'String with spaces';
+ '' = 'Empty string';
+ */
+```
+
+2.3 You can also create and assign (preferably not in one go or declaration separated by commas)
+```javascript
+const myObj = new Object();
+const key = 'Alicia';
+const obj = new Object();
+const rand = Math.ceil();
+const num = 9007199254740991;
+
+// and then set values
+myObj.someProp = 'Some prop';
+myObj[key] = 'Keys';
+myObj[obj] = 'Object';
+myObj[rand] = 'Not a number';
+myObj[num] = 'Max Safe Integer';
+
+/* ==>
+ someProp = 'Some prop';
+ Alicia = 'Keys';
+ [object object] = 'Object';
+ NaN = 'Not a number'
+ 9007199254740991 = 'Max Safe Integer';
+ */
+
+// and change the value using bracket notation as well as add using dot notation
+myObj['someProp'] = 'Some other Prop';
+myObj['Alicia'] = 'Not Keys?';
+myObj['9007199254740991'] = 'Changed max number value';
+myObj.newProp = 'New property';
+myObj.fluxProp = 'Flux property';
+myObj.float = 0.2;
+
+/* ==>
+ someProp = 'Some other prop';
+ Alicia = 'Not Keys?';
+ [object object] = 'Object';
+ NaN = 'Not a number'
+ 9007199254740991 = 'Changed max number value';
+ newProp = 'New property';
+ fluxProp = 'Flux property';
+ float = 0.2;
+ */
+
+```
--- object-array-patterns/object-creation.js
+++ object-array-patterns/object-creation.js
@@ -1,7 +1,93 @@
 /**
- * Conditionals
- * JavaScript Patterns - modern JS patterns with ES5 & ES6 examples
+ * CH. 02 - Essential Patterns
+ * Object Creation
  *
- * @copyright 2016 Ahad Bokhari
+ * JavaScript Patterns - modern JS patterns with ES5 & ES6 examples
+ * @copyright 2016 - 2017, Ahad Bokhari
  * license: MIT
+ */
+
+/**
+ * (i.) JavaScript is designed around objects, a simple design pattern you should know well. An object is a collection of properties, and a property is an association
+ * between a name (or key) and a value. A property's value can be a function, in which case the property is known as a method. Objects are crucial, if you understand
+ * them, then you understand JavaScript
+ */
+
+// a. An object is similar to variables, except for the properties associated with and attached to the object
+
+const myObj = new Object();
+
+// b. You may assign properties, out of which each will hold a value using dot notation
+
+myObj.age = 12;
+myObj.height = '173cm';
+myObj.color = 'Fair';
+myObj.ethnicity = 'Moroccan';
+
+console.log(myObj);
+
+/* ==>
+ age = 12;
+ height = '173cm';
+ color = 'Fair';
+ ethnicity = 'Moroccan';
+ */
+
+// c. You can also assign property and values, also known as key/value pairs via bracket notation
+myObj['key'] = 'Alicia';
+myObj['Always a string'] = 'String with spaces';
+myObj[''] = 'Empty string';
+
+console.log(myObj);
+
+/* ==>
+ age = 12;
+ height = '173cm';
+ color = 'Fair';
+ ethnicity = 'Moroccan';
+ key = 'Alicia';
+ Always a string = 'String with spaces';
+ '' = 'Empty string';
+ */
+
+// d. You can also create and assign (preferably not in one go or declaration separated by commas)
+const myObj = new Object();
+const key = 'Alicia';
+const obj = new Object();
+const rand = Math.ceil();
+const num = 9007199254740991;
+
+// and then set values
+myObj.someProp = 'Some prop';
+myObj[key] = 'Keys';
+myObj[obj] = 'Object';
+myObj[rand] = 'Not a number';
+myObj[num] = 'Max Safe Integer';
+
+/* ==>
+ someProp = 'Some prop';
+ Alicia = 'Keys';
+ [object object] = 'Object';
+ NaN = 'Not a number'
+ 9007199254740991 = Max Safe Integer;
+ */
+
+// and change the value using bracket notation as well as add using dot notation
+myObj['someProp'] = 'Some other Prop';
+myObj['Alicia'] = 'Not Keys?';
+myObj['9007199254740991'] = 'Changed max number value';
+myObj.newProp = 'New property';
+myObj.fluxProp = 'Flux property';
+
+console.log(myObj);
+
+/* ==>
+ someProp = 'Some other prop';
+ Alicia = 'Not Keys?';
+ [object object] = 'Object';
+ NaN = 'Not a number'
+ 9007199254740991 = 'Changed max number value';
+ newProp = 'New property';
+ fluxProp = 'Flux property';
+ float = 0.2;
  */
