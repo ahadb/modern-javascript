0aff3bed8d04d87964b56a78a9f5f80efcbbe577
Ahad Bokhari <ahadbokhari@gmail.com>
2017-03-14 22:48:59 -0400

    module pattern additions, code of conduct, etc.

--- object-patterns/module-pattern.js
+++ object-patterns/module-pattern.js
@@ -8,11 +8,77 @@
  */
 
 /*
- * To understand what a Module is, you’ll need to understand what an IIFE
+ * (i). To understand what a Module is, you’ll need to understand what an IIFE
  * can do for you and how it's constructed
  */
 
+// a. Simple iife, or immediately invoked functional expression
 (function() {
   // ...code here
 })();
 
+// b. Create a namespace for our code or anonymous module
+var myModule = (function(idx) {
+  idx.indexOf(idx);
+  // ...code here
+})();
+
+/*
+ * (ii.) Modules in JavaScript are used to create private and public encapsulation of classes - those
+ * seen in conventional programming languages. the module pattern internalizes state and keeps code shielded
+ * from the global scope, and potentially other libraries and variables that can collide with it.
+ */
+
+// a. So, to mimic a private method or property one has to only do this
+var aModule = (function() {
+  var privateProp = 'Private Property';
+  var privateMethod = function() {
+    console.log('I\'m a private method');
+  }
+})();
+
+// b. Now we simply use the `return` statement to return an object literal to which
+// we bind the methods we want to expose to the namespace. It's that simple
+var aModule = (function() {
+  var privateProp = 'Private Property';
+  var privateMethod = function() {
+    console.log('I\'m a private method');
+  };
+
+  // the return statement
+  return {
+    privateProp: privateProp,
+    privateMethod: privateMethod
+  }
+})();
+
+// b. Now we simply use the `return` statement to return an object literal to which
+// we bind the methods we want to expose to the namespace. It's that simple
+var aModule = (function() {
+  var privateProp = 'Private Property';
+  var privateMethod = function() {
+    console.log('I\'m a private method');
+  };
+  // the return statement
+  return {
+    privateProp: privateProp,
+    privateMethod: privateMethod
+  }
+})();
+
+//. c. Perhaps a more readable variation could look like this
+var Module = (function () {
+
+  // locally scoped Object
+  var myObject = {};
+
+  // declared with `var`, must be "private"
+  var privateMethod = function () {};
+
+  myObject.someMethod = function () {
+    // take it away Mr. Public Method
+  };
+
+  return myObject;
+
+})();
