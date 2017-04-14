720339b4947ea15c14ea7270a4edc63d3ad25804
Ahad Bokhari <ahadbokhari@gmail.com>
2017-04-14 19:37:28 -0400

    private members in classes

--- object-patterns/class-pattern.js
+++ object-patterns/class-pattern.js
@@ -178,4 +178,64 @@ var mapped = a.map(x => x * x);
 console.log(mapped instanceof MyArray); // => false
 console.log(mapped instanceof Array);   // => true
 
+/* (v). ES5 patterns brought us numerous ways to keep our data private, let's take a look at how we
+ * we can do the same in ES6 classes. There is no way to natively do this, in either ES5 or ES6. We c
+ * can accomplish this however by using using specific patterns. None is wholly superior might I add.
+ */
 
+// a. Private data with naming conventions. Our data is safe, but we could have clashing private
+// property naming clashes. Also this isn't elegant, everything is globbed within the constructorß
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+    this.setName = function(name) {
+      _name = name;
+    };
+    this.getName = function() {
+      return _name;
+    };
+    this.getAge = function() {
+      return _age
+    };
+  }
+}
+
+// b. You can use Symbols for private data whose properties you don't want to expose. The trade-off
+// here is that you can use reflection and expose all property keys. It's a somewhat private way.
+const name = Symbol('name');
+const age = Symbol('age');
+
+class Person {
+  constructor(name, age) {
+    this[_name] = name;
+    this[_age] = age;
+  }
+}
+const p = new Person('Raul', 40);
+
+console.log(Object.keys(p)); // => []
+console.log(Reflect.ownKeys(c));// => [ Symbol(name), Symbol(age) ]
+
+// c. Using WeakMaps is probably your best bet in a world of half answers
+let Person = (function () {
+  let privateProps = new WeakMap();
+
+  class Person {
+    constructor(name) {
+      this.name = name; // this is public
+      privateProps.set(this, {age: 20}); // this is private
+    }
+    greet() {
+      // Here we can access both name and age
+      console.log(`name: ${this.name}, age: ${privateProps.get(this).age}`);
+    }
+  }
+
+  return Person;
+})();
+
+let joe = new Person('Joe');
+joe.greet();
+
+// here we can access joe's name but not age
